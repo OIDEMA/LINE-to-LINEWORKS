ab34c951eba989ff53df03ca814c6e6fa089cabf
OIDEMA <k-goda@oidema-jp.com>
2022-01-12 15:27:16 +0900

    envの修正

--- index.js
+++ index.js
@@ -73,9 +73,9 @@ app.post("/callback", function(req, res) {
 app.get("/accountSet", function() {
 
     const oauth2Client = new google.auth.OAuth2(
-        process.env.YOUR_CLIENT_ID,
-        process.env.YOUR_CLIENT_SECRET,
-        process.env.YOUR_REDIRECT_URL
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URL
     );
 
     /* スコープ一覧 https://developers.google.com/identity/protocols/oauth2/scopes#adsense */
