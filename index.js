c2a60ff15ac3c59873adcc2d2a1dc655c81c045b
OIDEMA <k-goda@oidema-jp.com>
2022-01-26 18:50:32 +0900

    request確認

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const https = require("https")
+
 const express = require("express")
 const app = express()
 const { google } = require('googleapis');
 
-/* コンフィグ設定 */
-require('dotenv').config()
+/* refresh token取得のために追加 */
+const http = require('http');
+const querystring = require('querystring');
+const opn = require('opn');
+
+
+const querystring = require('querystring');
+const opn = require('opn');
+
 
 const PORT = process.env.PORT || 3000
 const TOKEN = process.env.LINE_ACCESS_TOKEN
@@ -26,6 +34,7 @@ app.listen(PORT, () => {
 
 app.post("/callback", function(req, res) {
     if (req.body.events[0].type === "message") {
+        console.log(req)
         // Message data, must be stringified
         const dataString = JSON.stringify({
           replyToken: req.body.events[0].replyToken,
@@ -81,6 +90,7 @@ app.get("/accountSet", function() {
     /* スコープ一覧 https://developers.google.com/identity/protocols/oauth2/scopes#adsense */
     const scopes = [
         'https://www.googleapis.com/auth/adsense.readonly',
+        'https://www.googleapis.com/auth/plus.me'
     ];
 
     const url = oauth2Client.generateAuthUrl({
@@ -91,24 +101,46 @@ app.get("/accountSet", function() {
         scope: scopes
       });
 
-      console.log(url)
+      const tokens = await oauth2Client.getToken(code)
+      oauth2Client.setCredentials(tokens);
 
-    //   const tokens = await oauth2Client.getToken(code)
-    //   oauth2Client.setCredentials(tokens);
+      oauth2Client.on('tokens', (tokens) => {
+        if (tokens.refresh_token) {
+          // store the refresh_token in my database!
+          console.log(tokens.refresh_token);
+        }
+        console.log(tokens.access_token);
+      });
 
-    //   oauth2Client.on('tokens', (tokens) => {
-    //     if (tokens.refresh_token) {
-    //       // store the refresh_token in my database!
-    //       console.log(tokens.refresh_token);
-    //     }
-    //     console.log(tokens.access_token);
-    //   });
-
-    //   oauth2Client.setCredentials({
-    //     refresh_token: `STORED_REFRESH_TOKEN`
-    //   });
+      oauth2Client.setCredentials({
+        refresh_token: `STORED_REFRESH_TOKEN`
+      });
     })
 
+    function getRefreshToken(){
+        const server = http.createServer(async (req, res) => {
+            if (req.url.indexOf('/oauth2callback') > -1) {
+              // acquire the code from the querystring, and close the web server.
+              const qs = querystring.parse(url.parse(req.url).query);
+              console.log(`Code is ${qs.code}`);
+              res.end('Authentication successful! Please return to the console.');
+              server.close();
+              
+              // Now that we have the code, use that to acquire tokens.
+              const r = await oAuth2Client.getToken(qs.code);
+              
+              // Make sure to set the credentials on the OAuth2 client.
+              oAuth2Client.setCredentials(r.tokens);
+              console.info('Tokens acquired.');
+              resolve(oAuth2Client);
+            }
+          }).listen(3000, () => {
+            // open the browser to the authorize url to start the workflow
+            opn(authorizeUrl);
+          });
+    }
+
+      
 
 
 /* Reference */
