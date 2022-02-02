4b15283b6da349e3d83a1cffa6049e16f71bc576
OIDEMA <k-goda@oidema-jp.com>
2022-02-02 12:48:59 +0900

    delete

--- index.js
+++ index.js
@@ -30,13 +30,12 @@ app.listen(PORT, () => {
 app.post("/callback", function(req, res) {
     if (req.body.events[0].type === "message") {
         // Message data, must be stringified
-        console.log(req.body.events[0].message)
         const dataString = JSON.stringify({
           replyToken: req.body.events[0].replyToken,
           messages: [
             {
               "type": "text",
-              "text": "May I help you?"
+              "text": req.body.events[0].message.text
             }
           ]
         })
@@ -47,14 +46,14 @@ app.post("/callback", function(req, res) {
           "Authorization": "Bearer " + TOKEN
         }
     
-        // Options to pass into the request
-        // const webhookOptions = {
-        //   "hostname": "api.line.me",
-        //   "path": "/v2/bot/message/reply",
-        //   "method": "POST",
-        //   "headers": headers,
-        //   "body": dataString
-        // }
+        //Options to pass into the request
+        const webhookOptions = {
+          "hostname": "api.line.me",
+          "path": "/v2/bot/message/reply",
+          "method": "POST",
+          "headers": headers,
+          "body": dataString
+        }
     
         // Define request
         const request = https.request(webhookOptions, (res) => {
