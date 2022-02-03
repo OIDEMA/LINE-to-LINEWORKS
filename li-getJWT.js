85fe41338530c4f8ad92b7923f1a4894db2ef626
OIDEMA <k-goda@oidema-jp.com>
2022-02-03 14:11:15 +0900

    修正

--- li-getJWT.js
+++ li-getJWT.js
@@ -1,23 +1,23 @@
 const jose = require('node-jose');
 
 module.exports = function getJWT() {
-
-  const header = {
-    alg: "RS256",
-    typ: "JWT",
-    kid: process.env.KID
-  },
-
-  const payload = {
+  const jwt = jose.JWS.createSign(
+    {
+      format: 'compact',
+      fields: {
+        alg: "RS256",
+        typ: "JWT",
+        kid: process.env.KID
+      }
+    },
+    JSON.parse(process.env.LI_PRIVATEKEY))
+  .update(JSON.stringify({
     iss: process.env.CHANNELID,
     sub: process.env.CHANNELID,
     aud: "https://api.line.me/",
     exp: Math.floor(new Date().getTime() / 1000) + 60 * 30,
     token_exp: 60 * 60 * 24 * 30
-  },
-
-  const jwt = jose.JWS.createSign({format: 'compact', fields: header}, JSON.parse(process.env.LI_PRIVATEKEY))
-  .update(JSON.stringify(payload))
+  }))
   .final()
   .then((res)=> {
     console.log(res);
