a4f08c7945b9540cf000325af56117d1daa827e3
OIDEMA <k-goda@oidema-jp.com>
2022-02-03 16:28:42 +0900

    修正

--- li-sendToCustomer.js
+++ li-sendToCustomer.js
@@ -6,29 +6,6 @@ const getServerToken = require("./get-server-token");
 
 module.exports = async function sendMessageToCustomer(message, newtoken, accountId, companyUser) {
 
-  const client = new line.Client({
-    channelAccessToken: newtoken
-  });
-
-  const replyUser = await getAccountInfo(companyUser)
-
-  const Message = {
-    type: 'text',
-    text: message + replyUser
-  };
-  
-  client.pushMessage(accountId, Message)
-    .then(() => {
-      console.log('メッセージを送信しました')
-    })
-    .catch((err) => {
-      console.log('エラーが発生しました')
-    }
-  );
-}
-
-/* Line Works User */
-async function getAccountInfo(sender) {
   getJWT(jwttoken => {
     getServerToken(jwttoken, async (newtoken) => {
       const account = await axios({
@@ -44,8 +21,27 @@ async function getAccountInfo(sender) {
       }).catch((err) => {
         console.log(`error is ${err}`)
       })
+      return "穴吹興産株式会社"+ "\n" + account.name + "\n" +　"所属部署：" + account.representOrgUnitName 
     })
-    return "穴吹興産株式会社"+ "\n" + account.name + "\n" +　"所属部署：" + account.representOrgUnitName 
+    
+    const client = new line.Client({
+      channelAccessToken: newtoken
+    });
+  
+    const replyUser = await getAccountInfo(companyUser)
+  
+    const Message = {
+      type: 'text',
+      text: message + replyUser
+    };
+    
+    client.pushMessage(accountId, Message)
+      .then(() => {
+        console.log('メッセージを送信しました')
+      })
+      .catch((err) => {
+        console.log('エラーが発生しました')
+      }
+    );
   });
-  return
 }
